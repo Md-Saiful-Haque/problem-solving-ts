4d6c9b27f1287a9849c12ef2512ec81a7ef9d142
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-06-11 16:11:34 +0600

    fizzBuzz solved

--- problem.d4.ts
+++ problem.d4.ts
@@ -14,3 +14,22 @@ function mergeObjects(obj1: object, obj2: object): object {
 }
 
 console.log(mergeObjects({ a: 1 }, { b: 2 }));
+
+
+// Problem 3: FizzBuzz
+
+function fizzBuzz(n: number): void {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz(15);
